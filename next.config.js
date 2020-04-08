1e8e4a9fae4a2edbc6278b066b5a5194b0983bd9
mathcw <964839421@qq.com>
2020-04-08 12:29:30 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withCss = require('@zeit/next-css');
 const isProd = process.env.NODE_ENV === 'production'
 const assetPrefix = isProd?'http://b2b.tongyeju.com/next':'';
-const basePath = isProd?'/next':'';
+const basePath = isProd?'':'';
 
 if (typeof require !== undefined){
     require.extensions['.css'] = file => {
